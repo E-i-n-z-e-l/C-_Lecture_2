be4a709c9bb1c3a2acbe0d82afb3e895bb9c0722
Einzel <einzel0102@mail.ru>
2022-12-20 16:28:35 +0800

    Задача 2 - введение в массивы

---
+++ Task02_Arroy/Program.cs
@@ -0,0 +1,18 @@
+﻿int Max(int argument1, int argument2, int argument3)
+{
+    int max_result = argument1;
+    if (argument2 > max_result) max_result = argument2;
+    if (argument3 > max_result) max_result = argument3;
+    return max_result;
+}
+
+int[] array = { 19, 28, 37, 46, 50, 64, 73, 28, 9 };
+
+int result = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
+Console.WriteLine(result);
+
+
